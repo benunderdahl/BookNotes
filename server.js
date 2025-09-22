460aeacd6125de85025dae01792338685a9a452a
benunderdahl <benjamin.underdahl@proton.me>
2025-09-22 01:21:35 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import pg from "pg"
+
+
+
+const app = express();
+const port = 3000;
+app.use(express.static('public'))
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`)
+})
