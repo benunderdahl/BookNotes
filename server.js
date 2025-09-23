04b8ce0281953f72c1b5f833a2169035e8fa6945
benunderdahl <benjamin.underdahl@proton.me>
2025-09-22 21:20:21 -0500

    connected front end to db and created add form for entering books to db

--- server.js
+++ server.js
@@ -1,21 +1,45 @@
 import express from "express"
 import pg from "pg"
+import axios from "axios"
+import bodyParser from "body-parser"
+import "dotenv/config"
 
+const api_endpoint = "https://covers.openlibrary.org/b/isbn/0385472579-L.jpg"
 
 
 const app = express();
 const port = 3000;
+const client = new pg.Client({
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    database: process.env.PG_DB,
+    port: process.env.PG_PORT,
+    password: process.env.PG_PASS
+})
+client.connect()
+
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true}))
 
 
-app.get("/", (req, res) => {
-    res.render("index.ejs")
+app.get("/", async (req, res) => {
+    const response = await client.query("SELECT * FROM books")
+    const books = response.rows
+    console.log(books)
+    res.render("index.ejs", {books})
 })
 
+app.get("/add", (req, res) => {
+    res.render("add.ejs")
+})
 
-
-
-
+app.post("/add", (req, res) => {
+    console.log(req.body.title)
+    console.log(req.body.isbn)
+    console.log(req.body.description)
+    console.log(req.body.summary)
+    res.redirect("/")
+})
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`)
